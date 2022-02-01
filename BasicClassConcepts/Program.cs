54482fd91f53f7fb24c5e77d002474c173c7c0f1
dnyaneshwar-madhewad <itz.dnyaneshwar@gmail.com>
2022-02-01 15:25:14 +0530

    Add project files.

---
+++ BasicClassConcepts/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicClassConcepts
+{
+    class Program
+    {
+        static void Main1()
+        {
+            System.Console.WriteLine("Hello Dnyaneshwar DAY 1 ");
+            System.Console.ReadLine();
+        }
+
+        static void Main()
+        {
+            Console.WriteLine("Hello Dnyaneshwar Block 2 ");
+            Console.ReadLine();
+        }
+
+
+    }
+}
